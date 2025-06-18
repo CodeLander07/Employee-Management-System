d5d841481f280152d5940f7708fc4187d0a89089
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-06-18 11:27:17 +0530

    feat: implement login functionality and add employee dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './components/Auth/login.jsx'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard.jsx'
 
 const App = () => {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <EmployeeDashboard />
     </>
   )
 }
--- src/components/Auth/login.jsx
+++ src/components/Auth/login.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const login = () => {
+
+const [email, setEmail] = useState("")
+const [password, setPassword] = useState("")
+
+
+const submitHandler = () =>{
+    e.preventDefault()
+    console.log("Email is: " ,email)
+    console.log("Password is: " ,password)
+    setEmail("")
+    setPassword("")
+}
+
   return (
     <div>
       <div className='flex h-screen w-screen items-center justify-center '>
-        <div className='border-2 border-red-600'>
-        <form action="" className='flex flex-col items-center justify-center'>
-            <input className=' text-xl text-black outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 rounded-full placeholder:text-white' type="email" placeholder='Enter Your Email' />
-            <input className=' text-xl text-black outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 rounded-full placeholder:text-white mt-6' type="password" placeholder='Enter Your password' />
-            <button className=' text-xl text-white outline-none border-none bg-emerald-600 py-3 px-5 rounded-full placeholder:text-white'>Log in</button>
+        <div className='border-2 border-emerald-600 rounded-xl p-20 '>
+        <form onSubmit={(e) =>{
+            submitHandler(e)
+        } } className='flex flex-col items-center justify-center'>
+            <input 
+            value ={email}
+            onChange={(e) =>{
+                setEmail(e.target.value)
+
+            }}
+            required className=' text-lg text-black outline-none bg-transparent border-2 border-emerald-600 py-2 px-6 rounded-full placeholder:text-grey-400' type="email" placeholder='Enter Your Email' />
+            <input 
+            value ={password}
+            onChange={(e) =>{
+                setPassword(e.target.value)
+
+            }} required className=' text-lg text-black outline-none bg-transparent border-2 border-emerald-600 py-2 px-6 rounded-full placeholder:text-grey-400 mt-6' type="password" placeholder='Enter Your password' />
+            <button className='mt-7 text-lg text-white outline-none border-none bg-emerald-600 py-2 px-8 rounded-full w-full placeholder:text-white'>Log in</button>
         </form>
         </div>
       </div>
